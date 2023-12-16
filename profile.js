c42b125d9fbf58f0cc5edfd3f7bb9a9110483443
Chumani-Lalasa <chumanilalasa@gmail.com>
2023-12-16 10:31:19 +0530

    readme

--- profile.js
+++ profile.js
@@ -1,5 +1,3 @@
-
-
 var userName = document.getElementById("userName");
 var nickName = document.getElementById("nickName");
 var submitBtn = document.getElementById("submit");
@@ -7,6 +5,6 @@ var submitBtn = document.getElementById("submit");
 submitBtn.onclick = () => {
   console.log("userName", userName.value);
   console.log("nickName", nickName.value);
-  window.location.href = "play.html";
+  window.location.href = "./play.html";
   return;   
  };
