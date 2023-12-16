a26660fec854ddc9f146321febea1e68be05c958
Chumani-Lalasa <chumanilalasa@gmail.com>
2023-12-16 19:01:38 +0530

    change

--- profile.js
+++ profile.js
@@ -2,9 +2,15 @@ var userName = document.getElementById("userName");
 var nickName = document.getElementById("nickName");
 var submitBtn = document.getElementById("submit");
 
-submitBtn.onclick = () => {
-  console.log("userName", userName.value);
-  console.log("nickName", nickName.value);
-  window.location.href = "./play.html";
-  return;   
- };
+// submitBtn.onclick = () => {
+//   console.log("userName", userName.value);
+//   console.log("nickName", nickName.value);
+//   window.location.href = "./play.html";
+//   return;   
+//  };
+
+submitBtn.onsubmit = (e) => {
+  e.preventDefault()
+  console.log(userName.value)
+  window.location.href="./play.html";
+}
